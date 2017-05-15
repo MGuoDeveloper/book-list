9e256dc88299f914d77ce4f433c991fde3a6a4b8
Meng Guo <mattguo2015@gmail.com>
2017-05-15 15:15:05 -0400

    book list and detail

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JavaScript: The Good Parts', pages: 41 },
+        { title: 'Harry Potter', pages: 12 },
+        { title: 'The Dark Tower', pages: 13 },
+        { title: 'Eloquent Ruby', pages: 31 },
+    ];
+}
